d1c0eb68b17a329a128902394206a4f6839dded6
Mhanmant <mr.ray655@gmail.com>
2017-08-28 19:47:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,16 @@ $input = json_decode(file_get_contents('php://input'), true);
 $sender = $input['entry'][0]['messaging'][0]['sender']['id'];
 $message = $input['entry'][0]['messaging'][0]['message']['text'];
 
-$message_to_reply = "HEllo";
+$message_to_reply = $message;
+print $message_to_reply;
 
 //API Url
 $url = 'https://graph.facebook.com/v2.6/me/messages?access_token='.$access_token;
+
+
 //Initiate cURL.
 $ch = curl_init($url);
+
 //The JSON data.
 $jsonData = '{
     "recipient":{
@@ -35,13 +39,17 @@ $jsonData = '{
 
 //Encode the array into JSON.
 $jsonDataEncoded = $jsonData;
+
 //Tell cURL that we want to send a POST request.
 curl_setopt($ch, CURLOPT_POST, 1);
+
 //Attach our encoded JSON string to the POST fields.
 curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
 //Set the content type to application/json
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
 //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+
 //Execute the request
 if(!empty($input['entry'][0]['messaging'][0]['message'])){
     $result = curl_exec($ch);
